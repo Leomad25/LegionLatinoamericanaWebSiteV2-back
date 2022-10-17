eb832c227584b2116c7b2922a680d5908a190dd2
Leonardo Miguel Aguado Diaz <leonardomiguelaguadodiaz@gmail.com>
2022-10-16 20:57:37 -0500

    init.

---
+++ src/main/java/com/api/LegionLatinoamericanaWebSiteV2back/LegionLatinoamericanaWebSiteV2BackApplication.java
@@ -0,0 +1,13 @@
+package com.api.LegionLatinoamericanaWebSiteV2back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegionLatinoamericanaWebSiteV2BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegionLatinoamericanaWebSiteV2BackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/LegionLatinoamericanaWebSiteV2back/LegionLatinoamericanaWebSiteV2BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.LegionLatinoamericanaWebSiteV2back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegionLatinoamericanaWebSiteV2BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
